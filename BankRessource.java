91b7017a0a7a4dcaddf1385e790d388090dbf156
ahcene <amahtout@enseirb-matmeca.fr>
2017-11-20 13:44:22 +0100

    update BankRessource

--- BankRessource.java
+++ BankRessource.java
@@ -2,12 +2,27 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.*;
     
 @Path("/bank")
-public class BankRessource{
+public class BankRessource
+{
     
-    public String createAccount();
+    @POST
+    @Path("/clients")
+    @Produces("application/xml")
+    public String createAccount()
+    {
+	System.out.println("Create account");
+	return "Success";
+    }
+    
+    @PUT
     public void deposit(float amount, String id_account);
+    
+    @PUT
     public void withdrawal(float amount, String id_account);
-    public float getBalance(String id_account);
+    
+    @GET
+    @Path("/clients/{id}")
+    public float getBalance(@PathParam("id") String id_account);
     public void intraTransfer(String id_src, String id_dst, float amount);
     public void interTransfer(String id_src, String id_dst, String bank_id, float amount);
 
